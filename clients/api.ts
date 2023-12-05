2f3d5bf558c2b6d592e78a575266fa05c84c5398
Deep Bhatia <deepbhatia1511@gmail.com>
2023-12-06 02:42:11 +0530

    changed_api_url

--- clients/api.ts
+++ clients/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request"
 
 const isClient = typeof window !== "undefined"
 
-export const graphqlClient = new GraphQLClient("http://localhost:8000/graphql", {
+export const graphqlClient = new GraphQLClient("https://d1899f7mzmayyt.cloudfront.net/graphql", {
    headers: () => ({
       Authorization: isClient ? `Bearer ${window.localStorage.getItem("_twitter_token")}` : "",
    })
